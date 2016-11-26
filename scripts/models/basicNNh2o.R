d65bf792f6d5236d1c3dc5e02bd78b85c89a608f
canghel <cvanghel@gmail.com>
2016-11-25 22:35:14 -0800

    incomplete nn work and ideas

--- scripts/models/basicNNh2o.R
+++ scripts/models/basicNNh2o.R
@@ -1,8 +1,15 @@
 ### basic NNh2o.R #############################################################
-# try making two 1024 -> 512 -> 256 1-layer autoencoders, with 256 layer then 
-# sent to a classifier; then maybe try to put it all together into 1024-512-256-2
-# with the weights initialized from the autoencoders
-# some code based on Yimin Hu's
+# trying just simplest idea 3) for now:
+#
+# ideas: 
+# 1) stacked autoencoder that decreases dim to 512-256 then goes down to 2 
+# classifier
+# 	- problem: weighing different samples? (only train with fracZero > something)
+# 	- can't do customisable NN (multivariate) using h2o
+# 2) two autoencoders, one for 0's and one for 1's
+#	- run new sample in both, then compare errors to determine the class 
+#	(method of comparing to other 1's and 0's signals?) 
+# 3) simplest: just 1024-(something)-2 classifier 
 
 ### PREAMBLE ##################################################################
 
@@ -21,7 +28,7 @@ ae <- list();
 
 args <- list(
 	activation = "Tanh", #activation function
-	epochs = 1000, #training rounds
+	epochs = 20, #training rounds
 	l1 = 1e-4,  #l1 penalty
 	adaptive_rate = TRUE, #whether to modify learning rate automatically
 	regression_stop = 1e-6, #early stopping
@@ -31,28 +38,50 @@ args <- list(
 
 set <- 1;
 
-# load data for given data set
+### LOAD AND PREPARE DATA #####################################################
+
+basicInfo <- read.delim( 
+	file = file.path("..", "..", "output", "dataProcessing", "2016-11-25_trainBasicInfo.csv"),
+	sep = ",",
+	stringsAsFactors = FALSE
+    );
+
 source("loadAvgFreqData.R");
-trainingData <- as.h2o(trainData);
-trainingResponse <- as.h2o(trainBasicInfo$response);
+browser();
+
+temp <- merge(trainData, trainBasicInfo[, c("response", "weights")], by="row.names")
+rownames(temp) <- temp[,1];
+temp <- temp[,-1];
+predictors <- setdiff(names(temp), "response")
+response <- temp[,"response"];
+trainData <- temp[, -which(colnames(temp)=="response")];
+
+trainingData <- as.h2o(trainData[1:10,]);
+
+### TRAIN #####################################################################
 
 aeModels <- vector();
 
-for (j in 1:2) {
-	ae <- do.call(h2o.deeplearning, 
-		modifyList(list(
-				x = names(trainingData),
-				training_frame = trainingData,
-				autoencoder = TRUE,
-				hidden = layerSizes[j]),
-				args)
-			);
+# am actually going to do this the long way...
+# train first autoencoder ----------------------------------------------------#
 
-	print(paste("j = ",j,", Number of epochs: ", last(ae@model$scoring_history$epochs)))
-	trainingData <- h2o.deepfeatures(ae,trainingData,layer=1)
-	names(trainingData) <- gsub("DF", paste0("L",j,sep=""), names(trainingData))
-	aeModels <- c(aeModels, ae)
-}
+j <- 1;
+
+ae <- do.call(h2o.deeplearning, 
+	modifyList(list(
+			x = predictors,
+			training_frame = trainData,
+			autoencoder = TRUE,
+			weights_column = "weights",
+			hidden = layerSizes[j]),
+			autoencoder = TRUE,
+			args)
+		);
+
+print(paste("j = ",j,", Number of epochs: ", last(ae@model$scoring_history$epochs)))
+trainingData <- h2o.deepfeatures(ae,trainingData,layer=1)
+names(trainingData) <- gsub("DF", paste0("L",j,sep=""), names(trainingData))
+aeModels <- c(aeModels, ae)
 
 w1.1 <- as.matrix(h2o.weights(aeModels[[1]], matrix_id=1));
 w1.2 <- as.matrix(h2o.weights(aeModels[[1]], matrix_id=2));
@@ -64,24 +93,24 @@ w2.2 <- as.matrix(h2o.weights(aeModels[[2]], matrix_id=2));
 biases2.1 <- as.matrix(h2o.biases(aeModels[[2]], vector_id=1));
 biases2.2 <- as.matrix(h2o.biases(aeModels[[2]], vector_id=2));
 
-temp <- cbind(as.matrix(trainingData), ifelse(trainBasicInfo$response, "Y", "N"));
-colnames(temp)[ncol(temp)] <- "response";
-temp <- as.h2o(temp)
+# temp <- cbind(as.matrix(trainingData), ifelse(trainBasicInfo$response, "Y", "N"));
+# colnames(temp)[ncol(temp)] <- "response";
+# temp <- as.h2o(temp)
 
-lastModel <- h2o.deeplearning(
-	x = 1:256,
-	training_frame = temp, 
-	y = 257,
-	hidden = 10,
-	#balance_classes = TRUE,
-	epochs = 100 #,
-	#classification_stop = 0.99
-)
-summary(lastModel)
+# lastModel <- h2o.deeplearning(
+# 	x = 1:256,
+# 	training_frame = temp, 
+# 	y = 257,
+# 	hidden = 10,
+# 	#balance_classes = TRUE,
+# 	epochs = 100 #,
+# 	#classification_stop = 0.99
+# )
+# summary(lastModel)
 
-path <- h2o.saveModel(lastModel, path=file.path("..", "..", "output", "basicNNh2o"), force=TRUE)
+# path <- h2o.saveModel(lastModel, path=file.path("..", "..", "output", "basicNNh2o"), force=TRUE)
 
-save(w1.1, w1.2, biases1.1, biases1.2, 
-	 w2.1, w2.2, biases2.1, biases2.2,
-	file = file.path("..", "..", "output", "-basicNNh2o-weights.RData"))
-	);
+# save(w1.1, w1.2, biases1.1, biases1.2, 
+# 	w2.1, w2.2, biases2.1, biases2.2,
+# 	file = file.path("..", "..", "output", paste0(Sys.Date(), "-basicNNh2o-weights.RData"))
+# 	);
--- scripts/models/loadAvgFreqData.R
+++ scripts/models/loadAvgFreqData.R
@@ -1,14 +1,8 @@
-trainBasicInfo <- read.delim( 
-	file = file.path("..", "..", "output", "dataProcessing", paste0("2016-10-22-train_", set, "_basicInfo.csv")),
-	sep = ",",
-	stringsAsFactors = FALSE
-    );
+### TRAIN DATA #############################################################
 
-# re-format feature data
-trainData <-  readMat(file.path("..", "..", "output", "dataProcessing", paste0('20161022_train_', set, '_avgFreq.mat')));
-files <- unlist(lapply(trainData$allFiles, "[[", 1));
-trainData <- trainData$avgFreq;
-rownames(trainData) <- files;
+trainBasicInfo <- basicInfo[grep(paste0('^', set, '_'), basicInfo$files),];
+# get feature
+load(file.path("..", "..", "output", "dataProcessing", paste0('2016-11-25_train_', set, '_avgFreq.RData')));
 
 # make rownames match
 trainBasicInfo <- trainBasicInfo[match(rownames(trainData), trainBasicInfo[,1]),]
@@ -17,7 +11,21 @@ rownames(trainBasicInfo) <- trainBasicInfo[,1];
 # sanity check
 print(identical(trainBasicInfo$files,rownames(trainData)))
 
-testData <-  readMat(file.path("..", "..", "output", "dataProcessing", paste0('20161022_test_', set, '_avgFreq.mat')));
+### CALCULATE WEIGHTS #####################################################
+
+# readjust weights
+# not sure if this is the best right way to deal with imbalance and zeros...
+# weight by (1-fraction of zeros)*fraction of observations with that outcome
+numPreictal <- sum(trainBasicInfo$response);
+numBaseline <- length(trainBasicInfo$response) - numPreictal;
+baselineWeight <- numBaseline/length(trainBasicInfo$response);
+trainBasicInfo$outcomeWeight <- baselineWeight;
+trainBasicInfo$outcomeWeight[which(trainBasicInfo$response==1)] <- 1- baselineWeight;
+trainBasicInfo$weights <- (1-trainBasicInfo$fracZero)*trainBasicInfo$outcomeWeight;
+
+### TEST DATA #############################################################
+
+testData <-  readMat(file.path("..", "..", "output", "dataProcessing", paste0('20161125_test_', set, '_new_avgFreq.mat')));
 files <- unlist(lapply(testData$allFiles, "[[", 1));
 testData <- testData$avgFreq;
 rownames(testData) <- files;
